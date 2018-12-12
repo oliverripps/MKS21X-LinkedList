d3ca54508554471ae1bdb1d0429cd4387df764e3
Oliver Ripps <oripps@stuy.edu>
2018-12-12 15:37:28 -0500

    first template

---
+++ Node.java
@@ -0,0 +1,22 @@
+class Node{
+ private int data;
+ private Node next,prev;
+
+ public Node(Node referencefrom, int num,Node referenceto){
+   data=num;
+   next=reference;
+   prev=referencefrom;
+ }
+ class MyLinkedList{
+ private int size;
+ private Node start,end;
+
+ public int size();
+ public boolean add(int value);
+ public String toString();
+}
+
+
+
+
+}
