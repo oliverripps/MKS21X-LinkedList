848684a8ce94bf1f3be59992dc1f39177f448ec3
Oliver Ripps <oripps@stuy.edu>
2018-12-13 11:46:22 -0500

    classwork 12/13

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,21 +7,41 @@ public MyLinkedList(){
 }
 
 /*public boolean add(int value){
+  Node end2=
 
-}
+}*/
 public int size(){
   return length;
 }
 public String toString(){
-
+  Node current=start;
+  String str = "{";
+  for(int i=0;i<length;i++){
+    str+=(current.next()).getData();
+    if(i<length-1){
+    str+=",";
+  }
+}
+  str+="}";
+  return str;
 }
 public Integer get(int index){
-
+  Node current=start;
+  for(int i=0;i<length;i++){
+    current=current.next();
+  }
+  return current.getData();
 }
 public Integer set(int index, Integer value){
-
+  Node current= start;
+  for(int i =0;i<index+1;i++){
+  current=current.next();
 }
-public boolean contains(Integer value){
+  Integer olddata=current.getData();
+  current.setData(value);
+  return olddata;
+}
+/*public boolean contains(Integer value){
 
 }
 public int indexOf(Integer value){
@@ -30,7 +50,7 @@ public int indexOf(Integer value){
 public void add(int index, Integer value){
 
 }
-public Integer remove(Integer value){
+public boolean remove(Integer value){
 
 }*/
 //EXCPETIONS!!!
--- Node.java
+++ Node.java
@@ -2,30 +2,33 @@ class Node{
  private int data;
  private Node next,prev;
 
- /*public Node(Node referencefrom, int num,Node referenceto){
+ public Node(Node referencefrom, int num,Node referenceto){
    data=num;
    next=referenceto;
    prev=referencefrom;
  }
  public Node next(){
+   return this.next;
 
  }
  public Node prev(){
-
+   return this.prev;
  }
  public void setNext(Node other){
-
+   this.next=other;
  }
  public void setPrev(Node other){
-
+   this.prev=other;
  }
  public Integer getData(){
-
+   return data;
  }
  public Integer setData(Integer i){
-
+   Integer olddata=data;
+   data=i;
+   return olddata;
  }
  public String toString(){
-
- }*/
+   return "" + data;
+ }
 }
