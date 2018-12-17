3f7c52154cc6361b797c36197eb8201930f33f07
Oliver Ripps <oripps@stuy.edu>
2018-12-17 11:32:49 -0500

    hw is to do extend

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -147,4 +147,9 @@ public void add(int index, Integer value){
       return true;
     }
   }
+  public void extend(MyLinkedList other){
+        //in O(1) runtime, move the elements from other onto the end of this
+        //The size of other is reduced to 0
+        //The size of this is now the combined sizes of both original lists
+    }
 }
